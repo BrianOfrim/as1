bbbccaee1d4011f73715771f334cfa7c1ee0e6d3
BrianOfrim <bofrim@ualberta.ca>
2016-09-28 13:12:33 -0600

    update menu

---
+++ app/src/main/java/com/example/brianofrim/as1/habitDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.brianofrim.as1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class habitDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_detail);
+    }
+}
